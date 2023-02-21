e2f93f7b0b394e43f5ea283a0281f3023151baae
samiuddin00 <samiuddinkhan2000@gmail.com>
2023-02-22 00:36:53 +0530

    react calculator project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+/* eslint-disable default-case */
+import { useReducer } from 'react';
+import DigitButton from './DigitButton';
+import OperationButton from './OperationButton';
 import './App.css';
+import './style.css'
+
+
+
+ export const ACTIONS = {
+  ADD_DIGIT:'add-digit',
+  CHOOSE_OPERATION:"choose-operation",
+  CLEAR:"clear",
+  DELETE_DIGIT:"delete-digit",
+  EVALUATE:"evaluate",
+}
+
+function reducer(state,{type , payload}){
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      if(payload.digit === "0" && state.currentValue === "0"){
+        return state
+      }
+      if(payload.digit === "." && state.currentValue.includes(".")){
+        return state
+      }
+      return {
+        ...state,
+        currentValue:`${state.currentValue || ""}${payload.digit}`,
+      }
+  }
+}
+
+
 
 function App() {
+  const [{currentValue,previousValue,operation}, dispatch] = useReducer(reducer,{})
+
+  // dispatch({type:ACTIONS.ADD_DIGIT,payload:{digit:1}})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-value">{previousValue} {operation}</div>
+        <div className="current-value">{currentValue}</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <OperationButton operation="/" dispatch={dispatch}/>
+      <DigitButton digit="1" dispatch={dispatch}/>
+      <DigitButton digit="2" dispatch={dispatch}/>
+      <DigitButton digit="3" dispatch={dispatch}/>
+      <OperationButton operation="*" dispatch={dispatch}/>
+      <DigitButton digit="4" dispatch={dispatch}/>
+      <DigitButton digit="5" dispatch={dispatch}/>
+      <DigitButton digit="6" dispatch={dispatch}/>
+      <OperationButton operation="+" dispatch={dispatch}/>
+      <DigitButton digit="7" dispatch={dispatch}/>
+      <DigitButton digit="8" dispatch={dispatch}/>
+      <DigitButton digit="9" dispatch={dispatch}/>
+      <OperationButton operation="-" dispatch={dispatch}/>
+      <OperationButton operation="." dispatch={dispatch}/>
+      <DigitButton digit="0" dispatch={dispatch}/>
+      <button className="span-two">=</button>
+
     </div>
   );
 }
